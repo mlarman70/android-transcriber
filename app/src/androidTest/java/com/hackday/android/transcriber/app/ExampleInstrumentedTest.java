fb16fe033fe39071efbaf9a35c9f3f1389c1afa9
Mark Larman <mark.larman@recordsure.com>
2017-06-15 23:02:00 +0100

    hackday Commit version that shows record/play to file

--- app/src/androidTest/java/com/hackday/android/transcriber/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hackday/android/transcriber/app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.hackday.android.transcriber.androidtranscriber", appContext.getPackageName());
+        assertEquals("com.hackday.android.transcriber.app", appContext.getPackageName());
     }
 }
